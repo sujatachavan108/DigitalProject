50164340dbf29cb3a4ecfa0c11f8bce7cf5867b9
sujata_chavan <sujatachavan108@gmail.com>
2022-06-15 15:08:16 +0530

    Received one

--- src/main/java/com/configuration/utils/OR.java
+++ src/main/java/com/configuration/utils/OR.java
@@ -1,13 +1,25 @@
 package com.configuration.utils;
 
-import java.util.Properties;
+import java.io.FileNotFoundException;
 
 public class OR {
 	String filePath="\\src\\main\\resources\\OR.properties";
 	PropertiesFile prop=new PropertiesFile();
+	public String  loginButton() throws FileNotFoundException {
+		
+		return  prop.getValue(filePath,"verifyLoginPage");
 
-	public String  getUserNameLocator() {
-		return prop.getValue(filePath,);
+	}
+	
+
+	public String  getUserNameLocator() throws FileNotFoundException {
+		//PropertiesFile prop=new PropertiesFile();
+		return prop.getValue(filePath,"verifyUsername");
       
 	}
+	public String getUserPassword() throws FileNotFoundException {
+		//PropertiesFile prop=new PropertiesFile();
+		return prop.getValue(filePath,"verifyPassword");
+
+	} 
 }
